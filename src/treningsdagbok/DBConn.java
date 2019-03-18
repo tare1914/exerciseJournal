1bade593749dc1679880677504dca43babf5df14
hannaaaa <hannaaaa@stud.ntnu.no>
2019-03-18 15:11:34 +0100

    Changed DBConn

--- src/treningsdagbok/DBConn.java
+++ src/treningsdagbok/DBConn.java
@@ -1,29 +1,24 @@
 package treningsdagbok;
 
-/**
- *
- * @author Svein Erik
- */
-
 import java.sql.*;
+import java.util.Scanner;
+import java.util.Date;
+import java.time.*;
 import java.util.Properties;
 
-public abstract class DBConn {
-    protected Connection conn;
-    public DBConn () {
+public class DBConn {
+	protected Connection conn;
+    public DBConn() {
     }
     public void connect() {
-    	try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            // Properties for user and password. Here the user and password are both '?'
+        try {
             Properties p = new Properties();
-            p.put("user", "myuser");
-            p.put("password", "mypassword");           
-	    //            conn = DriverManager.getConnection("jdbc:mysql://mysql.ansatt.ntnu.no/sveinbra_ektdb?autoReconnect=true&useSSL=false",p);
-            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/treningsdagbok?autoReconnect=true&useSSL=false",p);
+            p.put("user", "brukernavn");
+            p.put("password", "passord");
+            conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/brukernavn_database?autoReconnect=true&useSSL=false",p);
         } catch (Exception e)
-    	{
+        {
             throw new RuntimeException("Unable to connect", e);
-    	}
+        }
     }
 }
